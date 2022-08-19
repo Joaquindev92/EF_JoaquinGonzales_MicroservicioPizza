719ad7726a89d633df75acadcee67d5cecb1d4ef
Joaquindev92 <a1712802@idat.edu.pe>
2022-08-18 22:57:56 -0500

    ef

---
+++ src/test/java/com/idat/EF_JOAQUINGONZALES_MICROSERVICIOPIZZA/EfJoaquinGonzalesMicroservicioPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.EF_JOAQUINGONZALES_MICROSERVICIOPIZZA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfJoaquinGonzalesMicroservicioPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
